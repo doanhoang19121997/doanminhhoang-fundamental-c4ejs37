1ae3c7201d89a3f0d873f4168199169623af0524
hoang <doanhoang19121997@gmail.com>
2019-07-12 19:09:16 +0700

    initial commit

---
+++ session2/seriousEX.js
@@ -0,0 +1,25 @@
+// const n = prompt("your height(cm) is?");
+// const i = prompt("your weight(kg) is?");
+// let o = (n * 100); 
+// const k = (i / ( o * o ));
+// console.log(k);
+// if(k<16){
+//     console.log("Severely underweight!!!");
+//     alert("Severely underweight!!!");
+// }
+// else if(16<k<18.5){
+//     console.log("Underweight!!");
+//     alert("Underweight!!");
+// }
+// else if(18.5<k<25){
+//     console.log("Normal");
+//     alert("Normal");
+// }
+// else if(25<k<30){
+//     console.log("Overweight!!");
+//     alert("Overweight!!");
+// }
+// else if(k>30){
+//     console.log("Obese!!!");
+//     alert("Obese!!!");
+// }
